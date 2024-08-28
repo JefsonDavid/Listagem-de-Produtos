88b2ff91dd8cf01dcc4b95e112d67c5e26810be6
JefsonDavid <jefsondavid@gmail.com>
2024-08-27 21:58:40 -0300

    Add files via upload

--- Sistema de listagem de produtos com login/index.php
+++ Sistema de listagem de produtos com login/index.php
@@ -3,21 +3,36 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="styles/style.css">
     <title>Login</title>
 </head>
 <body>
-    <form method="POST" action="login_action.php">
-        <label>
-            Email:<br/>
-            <input type="email" name="email">
-        </label><br/>
-        <label>
-            Senha:<br/>
-            <input type="password" name="senha">
-        </label><br/><br/>
+    <div class="container">
+        <form method="POST" action="login_action.php">
+            <h1>Entrar na Plataforma</h1>
+            <div class="input-container">
+                <!-- <p>Email:</p><br/> -->
+                <input placeholder="Email" type="email" name="email" required>
+                <img width="20" height="20" src="https://img.icons8.com/ios/50/user--v1.png" alt="user--v1"/>
+            </div>
 
-        <input type="submit" value="Login"><br/>
-        <a href="adicionar.php">Ainda não sou cadastrado</a>
-    </form>
+            <div class="input-container">
+                <!-- <p>Senha:</p><br/> -->
+                <input placeholder="Senha"  type="password" name="senha" required>
+                <img width="20" height="20" src="https://img.icons8.com/ios/50/lock--v1.png" alt="lock--v1"/>
+                <a href="#">Esqueci minha senha</a>
+            </div>
+
+            <button type="submit" class="submit-button">Entrar</button><br/>
+
+            <div class="register-link">
+                <p>Ainda não está cadastrado? <a href="adicionar.php">Cadastrar</a></p>
+            </div>
+            
+        </form>
+    </div>
 </body>
 </html>
