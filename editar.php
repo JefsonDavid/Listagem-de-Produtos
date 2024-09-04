40567a7f29c3282c30a1571444ba6626a1c966a1
JefsonDavid <jefsondavid@gmail.com>
2024-09-03 22:27:24 -0300

    Add files via upload

--- editar.php
+++ editar.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <?php
         require 'config.php';
@@ -16,35 +16,38 @@
         }
 
         if($produtos === false) {
-            header("Location: listagem.php");
+            header("Location: teste.php");
             exit;
         }
     ?>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Edição de Produto</title>
+    <link rel="stylesheet" href="styles/editar_style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;700&display=swap" rel="stylesheet">
 </head>
 <body>
     <div>
+        <h1>Editar Produto</h1>
         <form method="POST" action="editar_action.php">
             <input type="hidden" name="id" values="<?= $produtos->getId(); ?>"/>
 
             <label>
                 Nome:<br/>
-                <input type="nome" name="nome" value="<?= $produtos->getNome() ?>"/>
+                <input type="text" name="nome" value="<?= $produtos->getNome(); ?>"/>
             </label><br/><br/>
 
             <label>
                 Quantidade:<br/>
-                <input type="quantidade" name="quantidade" value="<?= $produtos->getQuantidade() ?>">
+                <input type="text" name="quantidade" value="<?= $produtos->getQuantidade(); ?>">
             </label><br/><br/>
 
             <label>
                 Valor:<br/>
-                <input type="valor" name="valor" value="<?= $produtos->getValor() ?>">
+                <input type="text" name="valor" value="<?= $produtos->getValor(); ?>">
             </label><br/><br/>
 
-            <input type="submit" value="Salvar" />
+            <input class="btn" type="submit" value="Salvar" />
         </form>
     </div>
 </body>
